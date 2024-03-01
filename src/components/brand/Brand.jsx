95327498bc022e48f8cad6948ffe0d780589ca5b
RohaArslan <rohaarslan12@gmail.com>
2024-03-01 13:13:41 +0500

    commiting

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Brand.css';
+import { google, slack,atlassian,dropbox,shopify } from './import';
+export const Brand = () =>  (
+    <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+);
+
+export default Brand;
